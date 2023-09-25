e9254fcc4d0e40316b544496fe89732ef5cc3a2e
nayan911 <nayanagarwal911@gmail.com>
2023-09-25 18:32:06 +0530

    update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -70,6 +70,42 @@ const Login = () => {
             Do not have an account? <Link>Register Here</Link>
           </div>
         </div>
+        <div className="formstyle1">
+          <div className='form-list1'>
+            <div className='form-text'>
+              <span className='form-text-signin'>Sign In</span><br></br>
+              <span className='form-text-signin-account'>Sign in to your account</span>
+            </div>
+            <div className='form-sign'>
+            <GoogleOAuthProvider clientId="540670254248-n1d3qq95rh7t8li46njsu5m2hkkv5ndo.apps.googleusercontent.com">
+                <GoogleLogin 
+                  onSuccess={credentialResponse => {
+                    navigate('/home')
+                  }}
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                />
+            </GoogleOAuthProvider>
+                <button className='formapple'>
+                  <AiIcons.AiFillApple style={{fontSize: '15'}} /> sign in with apple
+                </button>
+            </div>
+            <div className='form-values'>
+                <Form layout="vertical" onFinish={onfinishHandler}>
+                    <Form.Item label="Email Address" name="email">
+                        <Input type="email" required />
+                    </Form.Item>
+                    <Form.Item label="Password" name="password">
+                        <Input type="password" required />
+                    </Form.Item>
+                    <Link to='/'>Forgot Password?</Link><br></br><br></br>
+                    <button className="btn btn-primary" type="submit">Sign In</button>
+                </Form>
+            </div>
+            Do not have an account? <Link>Register Here</Link>
+          </div>
+        </div>
     </section>
   )
 }
